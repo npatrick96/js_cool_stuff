335e29e0311c2d5ed2eae4ce95e409d467acd860
pniyongabo <patrickniyongabo@gmail.com>
2021-12-28 14:48:39 -0800

    added promises example that require installing node-fetch

---
+++ Asynchronous/promises.js
@@ -0,0 +1,6 @@
+import fetch from "node-fetch";
+
+fetch("https://api.randomuser.me/?nat=US&results=1") .then(res => res.json())
+.then(json => json.results)
+.then(console.log)
+.catch(console.error);
--- ObjectsAndArrays/spread_operator.js
+++ ObjectsAndArrays/spread_operator.js
@@ -1,16 +1,44 @@
-// 1. use spread operator to combine two arrays
-let peaks = ["Tallac", "Ralston", "Rose"]; let canyons = ["Ward", "Blackwood"];
+// 1. use spread operator to combine two arrays or two objects
+console.log("----------------------------");
+let peaks = ["Tallac", "Ralston", "Rose"];
+let canyons = ["Ward", "Blackwood"];
 let tahoe = [...peaks, ...canyons];
 console.log(tahoe.join(", ")); // Tallac, Ralston, Rose, Ward, Blackwood
+console.log("----------------------------");
+const morning = {
+  breakfast: "oatmeal",
+  brunch: "peanut butter and jelly",
+};
+const lunch = {
+    elevenses: "Bread Pudding with Cinnamon",
+    luncheon: "Beer and onion soup"
+};
+const dinner = "mac and cheese";
+const meals = { ...morning, ...lunch, dinner };
+console.log(meals);
+console.log("----------------------------");
 
 // 2. use spread operator to create a working copy of an array
-// let peaks = ["Tallac", "Ralston", "Rose"]; 
+// let peaks = ["Tallac", "Ralston", "Rose"];
 let [last] = peaks.reverse();
 console.log(last); // Rose
 console.log(peaks.join(", ")); // Rose, Ralston, Tallac
+console.log("----------------------------");
 
 // 3. use spread operator to destructure a bigger array into variables + small array
-const lakes = ["Donner", "Marlette", "Fallen Leaf", "Cascade"]; 
+const lakes = ["Donner", "Marlette", "Fallen Leaf", "Cascade"];
 const [first, ...others] = lakes;
 console.log(others.join(", ")); // Marlette, Fallen Leaf, Cascade
+console.log("----------------------------");
 
+// 5. use spread operator to pass an array of unkown size as argument to a function
+function directions(...args) {
+  let [start, ...remaining] = args;
+  let [finish, ...stops] = remaining.reverse();
+  console.log(`drive through ${args.length} towns`);
+  console.log(`start in ${start}`);
+  console.log(`the destination is ${finish}`);
+  console.log(`stopping ${stops.length} times in between`);
+}
+directions("Truckee", "Tahoe City", "Sunnyside", "Homewood", "Tahoma");
+console.log("----------------------------");
